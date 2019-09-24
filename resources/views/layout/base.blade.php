6ad3cd3e5551daceb1232190ac6e6f732091737d
Alberto <alberto_pumarejo@hotmail.com>
2019-09-24 11:25:23 -0500

    CRUD en back-end completamente funcional. Front-end todavia no es responsive

---
+++ resources/views/layout/base.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    @include('layout.partials.head')
+</head>
+<body>
+    <div class="container">
+        @yield('main')
+    </div>
+    <pre>
+        
+    </pre>
+
+    @include('layout.partials.footer-scripts')
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,6 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('showImage', 'UserController@showImage')->name('users.showImage');
+Route::resource('users', 'UserController');
